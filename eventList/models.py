e997cc0e520fd5ac7a3490ccf0cc81ea8337dd0f
logan-keede <68557630+logan-keede@users.noreply.github.com>
2023-10-29 19:52:19 +0530

    ss

--- eventList/models.py
+++ eventList/models.py
@@ -8,6 +8,7 @@ class Event(models.Model):
     link=models.CharField(max_length=300)
     start_date=models.DateTimeField()
     end_date=models.DateTimeField()
+    shown = models.BooleanField()
 
     def __str__(self) -> str:
         return self.title
