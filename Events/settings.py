66bd18aa75c05ca31bf92702ede465062bee7cd0
Ruminate <114932047+E-Cell-IIIT-Surat@users.noreply.github.com>
2023-11-08 14:20:33 +0530

    Update settings.py

--- Events/settings.py
+++ Events/settings.py
@@ -30,9 +30,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '')
 SECRET_KEY = 'django-insecure-%j+qyy3%o+0s!(^)wx!hqx6scmt1aknri5$c+_bc+8z1=w98)n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Application definition
